e8aedbfc07b7ccea203d52ef643aea637729146a
Kevin Von Seggern <noladrian@localhost.localdomain>
2020-05-14 15:46:49 -0500

    updated scripts to be more robust

--- trivia/scripts.py
+++ trivia/scripts.py
@@ -9,19 +9,20 @@ def import_game(file_path, game_name=None):
         name = game_name
     else:
         name = os.path.basename(file_path)
-    new_game = Game(game_title=name, pub_date=timezone.now())
-    new_game.save()
-    with open(file_path) as f:
-        csv_file = csv.reader(f)
-        round_dict = {}
-        for row in csv_file:
-            if row[0].lower() == 'final round':
-                fr = FinalRound(category=row[1], game=new_game, question=row[2], answer=row[3])
-                fr.save()
-            else:
-                if row[0] not in round_dict.keys():
-                    r = Round(category=row[0], game=new_game)
-                    r.save()
-                    round_dict[row[0]] = r
-                q = Question(round=round_dict[row[0]], question=row[1], answer=row[2])
-                q.save()
+    new_game = Game.objects.create(game_title=name, pub_date=timezone.now())
+    try:
+        with open(file_path) as f:
+            csv_file = csv.reader(f)
+            round_dict = {}
+            for row in csv_file:
+                if row[0].lower() == 'final round':
+                    FinalRound.objects.create(
+                        category=row[1], game=new_game, question=row[2], answer=row[3]
+                    )
+                else:
+                    if row[0] not in round_dict.keys():
+                        r = Round.objects.create(category=row[0], game=new_game)
+                        round_dict[row[0]] = r
+                    Question.objects.create(round=round_dict[row[0]], question=row[1], answer=row[2])
+    except:
+        new_game.delete()
